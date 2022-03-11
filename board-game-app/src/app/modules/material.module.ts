55fda7c3ca935176a4f8bfd972f59a196e9fb409
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2022-03-11 10:21:28 -0700

    finished unit 9

--- board-game-app/src/app/modules/material.module.ts
+++ board-game-app/src/app/modules/material.module.ts
@@ -4,6 +4,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list'
 import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
@@ -13,13 +18,21 @@ import { MatIconModule } from '@angular/material/icon'
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTooltipModule
   ],
   exports: [
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule { }
